b29eb063f5e2a8ea6146909adf1c42546b23e9f9
tanzhiwen <tanzhiwen@xiaohong.local>
2017-12-06 17:59:10 +0800

    Fixed: 删除之前的多余的东西，重新改造做一个yymodel的二次封装

---
+++ YYModelDesc/YYModelDesc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYModelDesc
+//
+//  Created by TANZHIWEN on 2017/12/6.
+//  Copyright © 2017年 TANZHIWEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYModelDesc/YYModelDesc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YYModelDesc
+//
+//  Created by TANZHIWEN on 2017/12/6.
+//  Copyright © 2017年 TANZHIWEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
